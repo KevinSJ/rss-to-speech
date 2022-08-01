921adceee7984ed7072b345b50d0d379a06bf41a
Kevin Jiang <KevinSJ@users.noreply.github.com>
2022-08-01 20:55:37 +1200

    refactor: Refactor to use a package for feed parsing

--- go.mod
+++ go.mod
@@ -2,22 +2,32 @@ module github.com/kevinsj/rss-to-podcast
 
 go 1.18
 
-require golang.org/x/net v0.0.0-20220526153639-5463443f8c37
+require (
+	cloud.google.com/go/texttospeech v1.3.0
+	golang.org/x/net v0.0.0-20220526153639-5463443f8c37
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+	google.golang.org/genproto v0.0.0-20220222213610-43724f9ea8cf
+)
 
 require (
 	cloud.google.com/go/compute v1.3.0 // indirect
-	cloud.google.com/go/texttospeech v1.3.0 // indirect
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/andybalholm/cascadia v1.1.0 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.7 // indirect
 	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/mmcdole/gofeed v1.1.3 // indirect
+	github.com/mmcdole/goxpp v0.0.0-20181012175147-0068e33feabf // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
 	go.opencensus.io v0.23.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/api v0.70.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20220222213610-43724f9ea8cf // indirect
 	google.golang.org/grpc v1.44.0 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 )
--- internal/types/atom.go
+++ internal/types/atom.go
@@ -1,49 +1,39 @@
 /*
- *rss-to-tts A progrm to read rss articles to tts mp3s
- *Copyright © 2022 Kevin Jiang
- *
- *Permission is hereby granted, free of charge, to any person obtaining
- *a copy of this software and associated documentation files (the "Software"),
- *to deal in the Software without restriction, including without limitation
- *the rights to use, copy, modify, merge, publish, distribute, sublicense,
- *and/or sell copies of the Software, and to permit persons to whom the
- *Software is furnished to do so, subject to the following conditions:
- *
- *The above copyright notice and this permission notice shall be included
- *in all copies or substantial portions of the Software.
- *
- *THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- *EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- *OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- *IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- *DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
- *TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
- *OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*rss-to-tts A progrm to read rss articles to tts mp3s
+*Copyright © 2022 Kevin Jiang
+*
+*Permission is hereby granted, free of charge, to any person obtaining
+*a copy of this software and associated documentation files (the "Software"),
+*to deal in the Software without restriction, including without limitation
+*the rights to use, copy, modify, merge, publish, distribute, sublicense,
+*and/or sell copies of the Software, and to permit persons to whom the
+*Software is furnished to do so, subject to the following conditions:
+*
+*The above copyright notice and this permission notice shall be included
+*in all copies or substantial portions of the Software.
+*
+*THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+*EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+*OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+*IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+*DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+*TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+*OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package types
 
 import (
 	"os"
-	"time"
 
 	"github.com/kevinsj/rss-to-podcast/internal/helpers"
+	"github.com/mmcdole/gofeed"
 
 	texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
 )
 
-//Feed struct for RSS
-type Feed struct {
-	Updated string  `xml:"updated"`
-	Entries []Entry `xml:"entry"`
-}
-
 //Create directory based on feed updated date
-func (f *Feed) CreateDirectory() (*string, error) {
-	feedUpdatedTime, err := time.Parse(time.RFC3339, f.Updated)
-	if err != nil {
-		return nil, err
-	}
-	directory := feedUpdatedTime.Format("2006-01-02")
+func CreateDirectory(f gofeed.Feed) (*string, error) {
+	directory := f.Title + " " + f.UpdatedParsed.Format("2006-01-02")
 
 	if err := os.MkdirAll(directory, 0755); err != nil {
 		return nil, err
@@ -52,36 +42,31 @@ func (f *Feed) CreateDirectory() (*string, error) {
 	return &directory, nil
 }
 
-//Entry struct for each Entry in the Feed
-type Entry struct {
-	ID        string `xml:"id"`
-	Title     string `xml:"title"`
-	Content   string `xml:"content"`
-	Published string `xml:"published"`
-	Summary   string `xml:"summary"`
-}
+func GetSynthesizeSpeechRequests(e *gofeed.Item) []*texttospeechpb.SynthesizeSpeechRequest {
+	text := e.Title + "\n\n" + helpers.StripHtmlTags(e.Content)
+	splitedText := helpers.Chunks(text, 4800)
 
-func (e *Entry) Sanitize() *Entry {
-	e.Content = helpers.StripHtmlTags(e.Content)
-	return e
-}
+	synthesizeRequest := make([]*texttospeechpb.SynthesizeSpeechRequest, 0)
 
-func (e *Entry) ToSynthesizeSpeechRequest() texttospeechpb.SynthesizeSpeechRequest {
-	text := e.Title + "\n" + e.Summary
-	return texttospeechpb.SynthesizeSpeechRequest{
-		// Set the text input to be synthesized.
-		Input: &texttospeechpb.SynthesisInput{
-			InputSource: &texttospeechpb.SynthesisInput_Text{Text: text},
-		},
-		// Build the voice request, select the language code ("en-US") and the SSML
-		// voice gender ("neutral").
-		Voice: &texttospeechpb.VoiceSelectionParams{
-			LanguageCode: "zh-CN",
-			SsmlGender:   texttospeechpb.SsmlVoiceGender_FEMALE,
-		},
-		// Select the type of audio file you want returned.
-		AudioConfig: &texttospeechpb.AudioConfig{
-			AudioEncoding: texttospeechpb.AudioEncoding_MP3,
-		},
+	for _, v := range splitedText {
+		req := texttospeechpb.SynthesizeSpeechRequest{
+			// Set the text input to be synthesized.
+			Input: &texttospeechpb.SynthesisInput{
+				InputSource: &texttospeechpb.SynthesisInput_Text{Text: v},
+			},
+			// Build the voice request, select the language code ("en-US") and the SSML
+			// voice gender ("neutral").
+			Voice: &texttospeechpb.VoiceSelectionParams{
+				LanguageCode: "zh-CN",
+				SsmlGender:   texttospeechpb.SsmlVoiceGender_FEMALE,
+			},
+			// Select the type of audio file you want returned.
+			AudioConfig: &texttospeechpb.AudioConfig{
+				AudioEncoding: texttospeechpb.AudioEncoding_MP3,
+			},
+		}
+
+		synthesizeRequest = append(synthesizeRequest, &req)
 	}
+	return synthesizeRequest
 }
