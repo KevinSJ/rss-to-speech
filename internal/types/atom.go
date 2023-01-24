f456271b40ded84ee7b17a45fa9da6aabfe03695
Kevin Jiang <KevinSJ@users.noreply.github.com>
2023-01-24 21:01:30 +1300

    Allow generating using Standard voice

--- internal/types/atom.go
+++ internal/types/atom.go
@@ -24,6 +24,7 @@ package types
 
 import (
 	"os"
+	"strings"
 
 	"github.com/kevinsj/rss-to-podcast/internal/helpers"
 	"github.com/mmcdole/gofeed"
@@ -31,29 +32,54 @@ import (
 	texttospeechpb "google.golang.org/genproto/googleapis/cloud/texttospeech/v1"
 )
 
+
+var VOICE_NAME_MAP_WAVENET = map[string]string{
+	"zh-CN": "cmn-CN-Wavenet-A",
+	"en-US": "en-US-Neural2-C",
+}
+
+var VOICE_NAME_MAP_STANDARD = map[string]string{
+	"zh-CN": "cmn-CN-Standard-D",
+	"en-US": "en-US-Standard-C",
+}
+
 // Create directory based on feed updated date
-func CreateDirectory(f gofeed.Feed) (*string, error) {
-	directory := f.Title + " " + f.UpdatedParsed.Format("2006-01-02")
+func CreateDirectory(f gofeed.Feed) (dir *string, err error) {
+
+	updatedDate := f.UpdatedParsed
+
+	if updatedDate == nil {
+		updatedDate = f.PublishedParsed
+	}
+
+	directory := f.Title + "_" + updatedDate.Local().Format("2006-01-02")
 
 	if err := os.MkdirAll(directory, 0755); err != nil {
 		return nil, err
 	}
 
+
 	return &directory, nil
 }
 
-func GetSynthesizeSpeechRequests(e *gofeed.Item) []*texttospeechpb.SynthesizeSpeechRequest {
-	text := e.Title + "\n\n"
-	if len(e.Content) > 0 {
-		text += helpers.StripHtmlTags(e.Content)
-	} else if len(e.Description) > 0 {
-		text += helpers.StripHtmlTags(e.Description)
+func GetSynthesizeSpeechRequests(e *gofeed.Item, lang string) []*texttospeechpb.SynthesizeSpeechRequest {
+	if len(lang) == 0 {
+		lang = "zh-CN"
 	}
-	splitedText := helpers.ChunksByte(text, 5000)
+
+	lang = func(s string) string {
+		s2 := strings.Split(s, "-")
+
+		return s2[0] + "-" + strings.ToUpper(s2[len(s2)-1])
+	}(lang)
+
+	feedContent := getFeedContent(e)
+
+	languageName := VOICE_NAME_MAP_STANDARD[lang]
 
 	synthesizeRequest := make([]*texttospeechpb.SynthesizeSpeechRequest, 0)
 
-	for _, v := range splitedText {
+	for _, v := range feedContent {
 
 		req := texttospeechpb.SynthesizeSpeechRequest{
 			// Set the text input to be synthesized.
@@ -63,7 +89,8 @@ func GetSynthesizeSpeechRequests(e *gofeed.Item) []*texttospeechpb.SynthesizeSpe
 			// Build the voice request, select the language code ("en-US") and the SSML
 			// voice gender ("neutral").
 			Voice: &texttospeechpb.VoiceSelectionParams{
-				LanguageCode: "zh-CN",
+				LanguageCode: lang,
+				Name:         languageName,
 				SsmlGender:   texttospeechpb.SsmlVoiceGender_FEMALE,
 			},
 			// Select the type of audio file you want returned.
@@ -75,4 +102,15 @@ func GetSynthesizeSpeechRequests(e *gofeed.Item) []*texttospeechpb.SynthesizeSpe
 		synthesizeRequest = append(synthesizeRequest, &req)
 	}
 	return synthesizeRequest
+}
+
+func getFeedContent(e *gofeed.Item) []string {
+	text := e.Title + "\n\n"
+	if len(e.Content) > 0 {
+		text += helpers.StripHtmlTags(e.Content)
+	} else if len(e.Description) > 0 {
+		text += helpers.StripHtmlTags(e.Description)
+	}
+	splitedText := helpers.ChunksByte(text, 5000)
+	return splitedText
 }
